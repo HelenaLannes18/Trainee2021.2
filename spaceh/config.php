cb8db922d2ad4c9bcd644dc5d3a1d27786bc1541
alvesmateus63 <alvesmateus.ps@gmail.com>
2021-12-10 11:29:43 -0300

    Altera o Banco de Dados

--- spaceh/config.php
+++ spaceh/config.php
@@ -2,7 +2,7 @@
 
 return [
     'database' => [
-        'name' => 'simula-spaceh',
+        'name' => 'krypton-moedas',
         'username' => 'root',
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
